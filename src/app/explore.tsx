66f16fa52d4a133b4674e6144c96bc6b4fd3f28a
4adonjic <4adonjic@gmail.com>
2026-09-21 09:28:42 +0200

    icônes des onglets

--- src/app/explore.tsx
+++ src/app/explore.tsx
@@ -489,4 +489,4 @@ export default function CarnetScreen() {
       )}
     </ScrollView>
   );
-}
+} 
--- src/components/app-tabs.tsx
+++ src/components/app-tabs.tsx
@@ -9,37 +9,26 @@ export default function AppTabs() {
     <NativeTabs
       backgroundColor={c.fond}
       indicatorColor={c.carteMeilleure}
+      iconColor={{ default: c.texteDoux, selected: c.orange }}
       labelStyle={{ selected: { color: c.orange } }}>
       <NativeTabs.Trigger name="index">
         <NativeTabs.Trigger.Label>Stations</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          src={require('@/assets/images/tabIcons/home.png')}
-          renderingMode="template"
-        />
+        <NativeTabs.Trigger.Icon sf="fuelpump.fill" md="local_gas_station" />
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="carte">
         <NativeTabs.Trigger.Label>Carte</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          src={require('@/assets/images/tabIcons/explore.png')}
-          renderingMode="template"
-        />
+        <NativeTabs.Trigger.Icon sf="map.fill" md="map" />
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="explore">
         <NativeTabs.Trigger.Label>Carnet</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          src={require('@/assets/images/tabIcons/explore.png')}
-          renderingMode="template"
-        />
+        <NativeTabs.Trigger.Icon sf="book.fill" md="menu_book" />
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="entretien">
         <NativeTabs.Trigger.Label>Entretien</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          src={require('@/assets/images/tabIcons/explore.png')}
-          renderingMode="template"
-        />
+        <NativeTabs.Trigger.Icon sf="wrench.and.screwdriver.fill" md="build" />
       </NativeTabs.Trigger>
     </NativeTabs>
   );
